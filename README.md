8f25970805b43b21b993db1460a6c3eb5253c33f
wangtao <lionel_tao@163.com>
2023-08-21 22:42:38 +0800

    提交

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # transport
+
+查看网页源代码搜索 externalId，拿到externalId 的值
+
+url: <https://www.youtube.com/channel/${externalId}>
--- monitor.py
+++ monitor.py
@@ -1,16 +1,17 @@
+import sys
+sys.path.append('./pytube')
 from pytube import Channel
 
 channel = Channel("https://www.youtube.com/channel/UCkQEYwVkQwA19O2D0n_clFg")
-print(channel)
 
-# video_urls = []
-# video_urls = channel.video_urls
-# print(video_urls)
+video_urls = []
+video_urls = channel.video_urls
+print(video_urls[0])
 
-# videos = []
-# videos = channel.videos
-# print(videos[0].title)
-# print(videos[0].length)
-# print(videos[0].streams.filter(res="720p"))
+videos = []
+videos = channel.videos
+print(videos[0].title)
+print(videos[0].length)
+print(videos[0].streams.filter(res="720p"))
 # videos[0].streams.filter(res="720p")[0].download(filename="latest.mp4")
 
